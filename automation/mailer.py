0b6d72beab47cd74420fb761726112c0ee7ed3b7
Andrew Tupper <andrew.s.tupper@gmail.com>
2021-08-09 13:42:14 -0700

    Added test

--- automation/mailer.py
+++ automation/mailer.py
@@ -2,13 +2,24 @@ import subprocess
 from email.mime.text import MIMEText
 
 
-sendmail = "/usr/sbin/sendmail"
+sendmail = "/sbin/sendmail"
 
 
 def sendEmail(from_address, to_address, subject, body):
+    # Create email message
     msg = MIMEText(body)
     msg['From'] = from_address
     msg['To'] = to_address
     msg['Subject'] = subject
 
+    # Send using subprocess
+    # -t = look for recipients in 'To'
+    # -oi = ignore single '.' in message which normally marks end
     subprocess.run([sendmail, "-t", "-oi"], input=msg.as_bytes())
+
+
+if __name__ == "__main__":
+    from_address = "andrew.s.tupper@gmail.com"
+    to_address = "andrew.tupper@oregonstate.edu"
+    subject = "test"
+    sendEmail(from_address, to_address, subject, "blah")
